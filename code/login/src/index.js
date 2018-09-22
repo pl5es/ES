59bd1aef019289bec1c8c8643e9afae47412dc66
jlamma <menesesdealmeida@gmail.com>
2018-09-22 18:47:31 +0100

    dummy news feed
    dummy news feed: ver screenshot no slack

--- code/login/src/index.js
+++ code/login/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from './Components/Login'
-import LoginFormik from './Components/LoginFormik'
+import Login from './Components/Login';
+import LoginFormik from './Components/LoginFormik';
+import DummyNewsFeed from './Components/DummyNewsFeed';
 
-ReactDOM.render(<LoginFormik />, document.getElementById('root'));
+//ReactDOM.render(<LoginFormik />, document.getElementById('root'));
+ReactDOM.render(<DummyNewsFeed />, document.getElementById('root'));
