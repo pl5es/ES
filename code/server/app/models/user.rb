3857e609982b1531ae39431adf8262345ff5b3f7
Tiago Martins <tiagomartins282@gmail.com>
2018-10-01 18:45:51 +0100

    add image upload for users

--- code/server/app/models/user.rb
+++ code/server/app/models/user.rb
@@ -1,14 +1,20 @@
 class User < ApplicationRecord
   has_secure_password
-
   has_many :interests
-
- validates_presence_of :username, :name, :orcid, :research_area, :institution
-
+  validates_presence_of :username, :name, :orcid, :research_area, :institution
   validates_uniqueness_of :username, :email, :orcid
 
+  mount_uploader :avatar, AvatarUploader
+
   def info
-    as_json(except: [:password_digest], include: :interests)
+    as_json(
+      except: [:password_digest, :avatar],
+      include: [:interests, {
+        avatar: {
+          only: :url,
+          methods: to_s
+        }
+      }])
   end
 
   def create_interests(params)
