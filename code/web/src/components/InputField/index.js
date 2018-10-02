beff0ce7ed540b448a2fc2d9fb0d3c515de10198
Alex Faria <afaria@student.uc.pt>
2018-10-02 19:05:55 +0100

    adicionar suporte para outros tipos que nao 'text'

--- code/web/src/components/InputField/index.js
+++ code/web/src/components/InputField/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const InputField = ({ label, field, form: { touched, errors }, ...props }) => (
   <div>
     <label>{label}</label>
-    <input type="text" {...field} {...props} />
+    <input {...field} {...props} />
     {touched[field.name] &&
       errors[field.name] && <div className="error">{errors[field.name]}</div>}
     <br />
